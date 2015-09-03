a4e0b44326dc85c441abc29efe2f650a176096d2
nakedmoon <tore.andrea@gmail.com>
2015-09-03 19:47:00 +0200

    MySQL Dump Skip Lock Tables Added

--- deploy/recipes/mysql-create-users.rb
+++ deploy/recipes/mysql-create-users.rb
@@ -8,25 +8,33 @@ node[:deploy].each do |application, deploy|
 
   mysql_command = "#{node[:mysql][:mysql_bin]} -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' : "-p#{node[:mysql][:server_root_password]}"}"
 
-  node[:mysql_import][:databases].each do |origin, db|
 
-    current_user = deploy[:database][:hyena_db_user][:username]
-    current_password = deploy[:database][:hyena_db_user][:password]
-    instances_ips = node["opsworks"]["layers"]["php-app"]["instances"].values.map{|i| i.fetch("private_ip")}
-    (instances_ips + deploy[:database][:hyena_db_user][:ips]).each do |ip|
+  current_user = deploy[:database][:hyena_db_user][:username]
+  current_password = deploy[:database][:hyena_db_user][:password]
+  instances_ips = node["opsworks"]["layers"]["php-app"]["instances"].values.map{|i| i.fetch("private_ip")}
+  user_ips = instances_ips.push(*deploy[:database][:hyena_db_user][:ips])
+  user_ips.each do |ip|
+    execute "create user #{current_user}@#{ip}" do
+      sql_user = Array.new.tap do |sql|
+        sql << "GRANT USAGE ON *.* TO '#{current_user}'@'#{ip}';"
+        sql << "DROP USER '#{current_user}'@'#{ip}';"
+        sql << "CREATE USER '#{current_user}'@'#{ip}' IDENTIFIED BY '#{current_password}';"
+      end.join
+      command "#{mysql_command} -e \"#{sql_user}\" "
+    end
+  end
+
+
+  node[:mysql_import][:databases].each do |origin, db|
+    user_ips.each do |ip|
       execute "grant all privileges on #{db} for user #{current_user}@#{ip}" do
-        sql_users = Array.new.tap do |sql|
-          sql << "GRANT USAGE ON *.* TO '#{current_user}'@'#{ip}';"
-          sql << "DROP USER '#{current_user}'@'#{ip}';"
-          sql << "FLUSH PRIVILEGES;"
-          sql << "CREATE USER '#{current_user}'@'#{ip}' IDENTIFIED BY '#{current_password}';"
+        sql_grant = Array.new.tap do |sql|
           sql << "GRANT ALL ON #{db}.* TO '#{current_user}'@'#{ip}' IDENTIFIED BY '#{current_password}';"
           sql << "FLUSH PRIVILEGES;"
         end.join
-        command "#{mysql_command} -e \"#{sql_users}\" "
+        command "#{mysql_command} -e \"#{sql_grant}\" "
       end
     end
-
   end
 
 
