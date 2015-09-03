904d9ff4baf2b292f8f611c5b874c6b6e663ad79
nakedmoon <tore.andrea@gmail.com>
2015-09-03 14:45:09 +0200

    Hyena DB Users

--- deploy/recipes/mysql-create-users.rb
+++ deploy/recipes/mysql-create-users.rb
@@ -7,23 +7,20 @@ node[:deploy].each do |application, deploy|
   next if deploy[:database].nil? || deploy[:database].empty?
 
   mysql_command = "#{node[:mysql][:mysql_bin]} -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' : "-p#{node[:mysql][:server_root_password]}"}"
-  mysql_dump_f = "mysqldump -h %s --user=%s --password=%s --add-drop-table %s | %s %s"
 
   node[:mysql_import][:databases].each do |origin, db|
 
-    log "====LOG====" do
-      message node[:opsworks].inspect
-      level :info
-    end
-
     instances_ips = node["opsworks"]["layers"]["php-app"]["instances"].values.map{|i| i.fetch("private_ip")}
     (instances_ips + deploy[:database][:hyena_db_user][:ips]).each do |ip|
-      execute "grant all privileges on #{db} for user #{deploy[:database][:hyena_db_user][:username]}@#{ip}" do
+      current_user = deploy[:database][:hyena_db_user][:username]
+      current_password = deploy[:database][:hyena_db_user][:password]
+      execute "grant all privileges on #{db} for user #{current_user}@#{ip}" do
         sql_users = Array.new.tap do |sql|
-          sql << "CREATE USER '#{deploy[:database][:hyena_db_user][:username]}'@'#{ip}' IDENTIFIED BY '#{deploy[:database][:hyena_db_user][:password]}';"
-          sql << "GRANT ALL ON #{db}.* TO '#{deploy[:database][:hyena_db_user][:username]}'@'#{ip}' IDENTIFIED BY '#{deploy[:database][:hyena_db_user][:password]}';"
+          sql << "DELETE FROM mysql.user WHERE User = '#{current_user}' AND Host = '#{ip}';FLUSH PRIVILEGES;"
+          sql << "CREATE USER '#{current_user}'@'#{ip}' IDENTIFIED BY '#{current_password}';"
+          sql << "GRANT ALL ON #{db}.* TO '#{current_user}'@'#{ip}' IDENTIFIED BY '#{current_password}';"
         end.join
-        command "#{mysql_command} -e '#{sql_users}' "
+        command "#{mysql_command} -e \"#{sql_users}\" "
       end
     end
 
