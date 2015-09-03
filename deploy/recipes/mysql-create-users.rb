1f9ae6800d7a5e0e49981a90203c1f89c272e44a
nakedmoon <tore.andrea@gmail.com>
2015-09-03 14:54:07 +0200

    Hyena DB Users

--- deploy/recipes/mysql-create-users.rb
+++ deploy/recipes/mysql-create-users.rb
@@ -10,13 +10,14 @@ node[:deploy].each do |application, deploy|
 
   node[:mysql_import][:databases].each do |origin, db|
 
+    current_user = deploy[:database][:hyena_db_user][:username]
+    current_password = deploy[:database][:hyena_db_user][:password]
     instances_ips = node["opsworks"]["layers"]["php-app"]["instances"].values.map{|i| i.fetch("private_ip")}
     (instances_ips + deploy[:database][:hyena_db_user][:ips]).each do |ip|
-      current_user = deploy[:database][:hyena_db_user][:username]
-      current_password = deploy[:database][:hyena_db_user][:password]
       execute "grant all privileges on #{db} for user #{current_user}@#{ip}" do
         sql_users = Array.new.tap do |sql|
-          sql << "DELETE FROM mysql.user WHERE User = '#{current_user}' AND Host = '#{ip}';FLUSH PRIVILEGES;"
+          sql << "GRANT USAGE ON *.* TO '#{current_user}'@'#{ip}';"
+          sql << "DROP USER '#{current_user}'@'#{ip}';"
           sql << "CREATE USER '#{current_user}'@'#{ip}' IDENTIFIED BY '#{current_password}';"
           sql << "GRANT ALL ON #{db}.* TO '#{current_user}'@'#{ip}' IDENTIFIED BY '#{current_password}';"
         end.join
