c61e92af5fb599e89b322f182ee9545b6dd41d01
Andrea Tore <a.tore@cbt.it>
2015-09-05 02:59:50 +0200

    Fix Sphinx.yml

--- deploy/recipes/php-migrate.rb
+++ deploy/recipes/php-migrate.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: deploy
-# Recipe:: rails-restart
+# Recipe:: php-migrate
 #
 
 include_recipe "deploy"
@@ -10,10 +10,16 @@ node[:deploy].each do |application, deploy|
     Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
     next
   end
+
+  log "check environment" do
+    message "Hyena Environment: #{ENV['HYENA_ENV']}"
+    level :info
+  end
+
+  php_env = ENV['HYENA_ENV'] || :development
   
   execute "migrate db with phinx" do
     cwd deploy[:current_path]
-    php_env = ENV['HYENA_ENV'] || :development
     command "php vendor/bin/phinx migrate -e #{php_env}"
     action :run
     
