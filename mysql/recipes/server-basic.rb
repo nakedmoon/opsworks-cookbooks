1fefb7dcd59c49944a060351019460d41400e2d0
nakedmoon <tore.andrea@gmail.com>
2015-09-09 18:10:48 +0200

    MySQL Dump Skip Lock Tables Added

---
+++ mysql/recipes/server-basic.rb
@@ -0,0 +1,19 @@
+require 'resolv'
+
+include_recipe 'mysql::client'
+include_recipe 'mysql::prepare'
+
+# for backwards compatiblity default the package name to mysql
+mysql_name = node[:mysql][:name] || "mysql"
+
+package "#{mysql_name}-server"
+
+include_recipe 'mysql::service'
+
+service "mysql" do
+  action :enable
+end
+
+service "mysql" do
+  action :start
+end
