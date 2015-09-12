a793c2761a6128e8e68e417e55ab59539522afff
nakedmoon <tore.andrea@gmail.com>
2015-09-12 19:42:37 +0200

    Mysql server AllInOne

--- deploy/recipes/mysql-import.rb
+++ deploy/recipes/mysql-import.rb
@@ -4,7 +4,6 @@ include_recipe 'deploy'
 Chef::Log.level = :debug
 
 node[:deploy].each do |application, deploy|
-  next if deploy[:database].nil? || deploy[:database].empty?
 
   mysql_command = "#{node[:mysql][:mysql_bin]} -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' : "-p#{node[:mysql][:server_root_password]}"}"
   mysql_dump_f = "mysqldump -h %s --user=%s --password=%s --add-drop-table --skip-lock-tables %s | %s %s"
