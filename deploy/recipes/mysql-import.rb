5abad9a11d6ce655a11c8a144377af7ec6c01e22
nakedmoon <tore.andrea@gmail.com>
2015-09-16 01:27:46 +0200

    Remote Mysql Connect

--- deploy/recipes/mysql-import.rb
+++ deploy/recipes/mysql-import.rb
@@ -6,7 +6,7 @@ Chef::Log.level = :debug
 node[:deploy].each do |application, deploy|
 
   mysql_command = "#{node[:mysql][:mysql_bin]} -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' : "-p#{node[:mysql][:server_root_password]}"}"
-  mysql_dump_f = "mysqldump -h %s --user=%s --password=%s --add-drop-table --skip-lock-tables %s | %s %s"
+  mysql_dump_f = "mysqldump -h %s --user=%s --password=%s --add-drop-table --skip-lock-tables --routines  --single-transaction --quick %s | %s %s"
 
   node[:mysql_import][:databases].each do |origin, db|
     execute "drop database #{db}" do
