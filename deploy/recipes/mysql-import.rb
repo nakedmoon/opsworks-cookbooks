76502fe238de1fcf2929cf58531d12b2ea4c0043
nakedmoon <tore.andrea@gmail.com>
2015-09-16 00:56:56 +0200

    Remote Mysql Connect

--- deploy/recipes/mysql-import.rb
+++ deploy/recipes/mysql-import.rb
@@ -5,7 +5,7 @@ Chef::Log.level = :debug
 
 node[:deploy].each do |application, deploy|
 
-  mysql_command = "#{node[:mysql][:mysql_bin]} -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' : "-p#{node[:mysql][:server_root_password]}"}"
+  mysql_command = "#{node[:mysql][:mysql_bin]} -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' : "-p#{node[:mysql][:server_root_password]}"}"
   mysql_dump_f = "mysqldump -h %s --user=%s --password=%s --add-drop-table --skip-lock-tables %s | %s | %s %s"
   mysql_definer_filter = "sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/'"
 
