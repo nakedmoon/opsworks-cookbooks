b0be99eaf4de5ec70bd74ddf276e5d4e9a654e2e
nakedmoon <tore.andrea@gmail.com>
2015-09-16 01:17:06 +0200

    Remote Mysql Connect

--- deploy/recipes/mysql-import.rb
+++ deploy/recipes/mysql-import.rb
@@ -6,9 +6,7 @@ Chef::Log.level = :debug
 node[:deploy].each do |application, deploy|
 
   mysql_command = "#{node[:mysql][:mysql_bin]} -u #{deploy[:database][:username]} #{node[:mysql][:server_root_password].blank? ? '' : "-p#{node[:mysql][:server_root_password]}"}"
-  mysql_dump_f = "mysqldump -h %s --user=%s --password=%s --add-drop-table --skip-lock-tables %s | %s | %s %s"
-  mysql_definer_filter = "sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/'"
-
+  mysql_dump_f = "mysqldump -h %s --user=%s --password=%s --add-drop-table --skip-lock-tables %s | %s %s"
 
   node[:mysql_import][:databases].each do |origin, db|
     execute "drop database #{db}" do
@@ -29,7 +27,7 @@ node[:deploy].each do |application, deploy|
       mysql_dump_command = sprintf(mysql_dump_f, node[:mysql_import][:host],
                                    node[:mysql_import][:username],
                                    node[:mysql_import][:password],
-                                   origin, mysql_definer_filter, mysql_command, db
+                                   origin, mysql_command, db
       )
       command mysql_dump_command
       action :run
