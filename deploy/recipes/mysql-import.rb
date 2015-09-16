180e516d8b4007d250b3740da5bd3a1f6ec85aa1
nakedmoon <tore.andrea@gmail.com>
2015-09-16 07:01:50 +0200

    Remote Mysql Connect

--- deploy/recipes/mysql-import.rb
+++ deploy/recipes/mysql-import.rb
@@ -9,6 +9,25 @@ node[:deploy].each do |application, deploy|
   mysql_dump_f = "mysqldump -h %s --user=%s --password=%s --add-drop-table --skip-lock-tables --routines  --single-transaction --quick %s | %s %s"
   instances_ips = node["opsworks"]["layers"]["php-app"]["instances"].values.map{|i| i.fetch("private_ip")}
 
+
+  deploy[:database][:hyena_db_users].each do |user|
+    current_user = user[:username]
+    current_password = user[:password]
+    user_ips = instances_ips.push(*user[:ips])
+
+    user_ips.each do |ip|
+      execute "create user #{current_user}@#{ip}" do
+        sql_user = Array.new.tap do |sql|
+          sql << "GRANT USAGE ON *.* TO '#{current_user}'@'#{ip}';"
+          sql << "DROP USER '#{current_user}'@'#{ip}';"
+          sql << "CREATE USER '#{current_user}'@'#{ip}' IDENTIFIED BY '#{current_password}';"
+        end.join
+        command "#{mysql_command} -e \"#{sql_user}\" "
+      end
+    end
+
+  end
+
   node[:mysql_import][:databases].each do |origin, db|
     execute "drop database #{db}" do
       command "#{mysql_command} -e 'DROP DATABASE `#{db}`' "
@@ -24,11 +43,11 @@ node[:deploy].each do |application, deploy|
       action :run
     end
 
-
     deploy[:database][:hyena_db_users].each do |user|
       current_user = user[:username]
       current_password = user[:password]
       user_ips = instances_ips.push(*user[:ips])
+
       user_ips.each do |ip|
         execute "grant all privileges on #{db} for user #{current_user}@#{ip}" do
           sql_grant = Array.new.tap do |sql|
@@ -39,19 +58,7 @@ node[:deploy].each do |application, deploy|
         end
       end
 
-      user_ips.each do |ip|
-        execute "create user #{current_user}@#{ip}" do
-          sql_user = Array.new.tap do |sql|
-            sql << "GRANT USAGE ON *.* TO '#{current_user}'@'#{ip}';"
-            sql << "DROP USER '#{current_user}'@'#{ip}';"
-            sql << "CREATE USER '#{current_user}'@'#{ip}' IDENTIFIED BY '#{current_password}';"
-          end.join
-          command "#{mysql_command} -e \"#{sql_user}\" "
-        end
-      end
-
     end
-
 
     execute "import remote database #{db}" do
       mysql_dump_command = sprintf(mysql_dump_f, node[:mysql_import][:host],
