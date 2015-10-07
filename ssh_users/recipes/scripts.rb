ed0712682c1117c8229c2da0d49b41414073860e
Andrea Tore <a.tore@cbt.it>
2015-10-08 00:36:38 +0200

    Hyena Scripts

--- ssh_users/recipes/scripts.rb
+++ ssh_users/recipes/scripts.rb
@@ -1,12 +1,19 @@
 group 'opsworks'
 
-node[:sftp_sites].each do |name, sftp|
-  template "/home/deploy/.ssh/#{name}.pem" do
-    backup false
-    source 'sftp.pem.erb'
-    owner node[:deploy][:user]
-    group node[:deploy][:group]
-    mode 0440
-    variables :private_key => sftp[:private_key]
+node[:deploy].each do |application, deploy|
+
+  node[:sftp_sites].each do |name, sftp|
+    template "/home/deploy/.ssh/#{name}.pem" do
+      backup false
+      source 'sftp.pem.erb'
+      owner deploy[:user]
+      group deploy[:group]
+      mode 0440
+      variables :private_key => sftp[:private_key]
+    end
   end
+
+
+
 end
+
