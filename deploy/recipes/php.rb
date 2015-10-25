694b0935a2ad7aec43043adba8f43b76648adc4e
nakedmoon <tore.andrea@gmail.com>
2015-10-25 13:34:24 +0100

    Configuration Hash

--- deploy/recipes/php.rb
+++ deploy/recipes/php.rb
@@ -26,7 +26,7 @@ node[:deploy].each do |application, deploy|
 
   node[:htaccess_deny].each do |dir|
     link_name = ::File.join(node[:deploy][application][:current_path], dir, '.htaccess')
-    link_dest = ::File.join(deploy[:deploy_to], 'shared', 'config', '.htaccess_deny')
+    link_dest = ::File.join(node[:deploy][application][:deploy_to], 'shared', 'config', '.htaccess_deny')
     link link_name do
       to link_dest
       action :create
