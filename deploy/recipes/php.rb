cddda8b5315ac23256714e1386959fbb227204b7
nakedmoon <tore.andrea@gmail.com>
2015-10-25 13:47:58 +0100

    Configuration Hash

--- deploy/recipes/php.rb
+++ deploy/recipes/php.rb
@@ -24,6 +24,15 @@ node[:deploy].each do |application, deploy|
     app application
   end
 
+  "#{deploy[:deploy_to]}/shared/export"
+
+  link ::File.join(node[:deploy][application][:current_path],'export') do
+    to "#{deploy[:deploy_to]}/shared/export"
+    action :create
+    link_type :symbolic
+    Chef::Log.debug("Linking export dir")
+  end
+
   node[:htaccess_deny].each do |dir|
     link_name = ::File.join(node[:deploy][application][:current_path], dir, '.htaccess')
     link_dest = ::File.join(node[:deploy][application][:deploy_to], 'shared', 'config', '.htaccess_deny')
