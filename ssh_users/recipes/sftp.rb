9a39f7b1be1bfe924d82ac0388f3ce6d630d9a7e
Raihan Mahmud Arman <raihan2006i@gmail.com>
2018-04-23 14:46:43 +0200

    Refactor ssh config to use single folder for both download and upload

--- ssh_users/recipes/sftp.rb
+++ ssh_users/recipes/sftp.rb
@@ -18,114 +18,53 @@ execute "create sftp base dir #{sftp_base_dir}" do
 end
 
 node[:sftp_sites].each do |sftp_site|
-  execute "add user #{sftp_site[:upload][:user]}" do
-    command "sudo adduser #{sftp_site[:upload][:user]}"
+  user "add user #{sftp_site[:user]}" do
+    username sftp_site[:user]
+    password sftp_site[:password] if sftp_site[:password].present?
+  end
+
+  execute "add .ssh dir for user #{sftp_site[:user]}" do
+    command "sudo su - #{sftp_site[:user]} -c \"mkdir -p .ssh\""
     action :run
   end
 
-  if sftp_site[:upload][:password].present?
-    user "add password for user #{sftp_site[:upload][:user]}" do
-      username sftp_site[:upload][:user]
-      password sftp_site[:upload][:password]
-      action :modify
-    end
-  end
-
-  execute "add user #{sftp_site[:download][:user]}" do
-    command "sudo adduser #{sftp_site[:download][:user]}"
+  execute "chmod .ssh dir for user #{sftp_site[:user]}" do
+    command "sudo su - #{sftp_site[:user]} -c \"chmod 700 .ssh\""
     action :run
   end
 
-  if sftp_site[:download][:password].present?
-    user "add password for user #{sftp_site[:download][:user]}" do
-      username sftp_site[:download][:user]
-      password sftp_site[:download][:password]
-      action :modify
-    end
-  end
-
-  execute "add .ssh dir for user #{sftp_site[:download][:user]}" do
-    command "sudo su - #{sftp_site[:download][:user]} -c \"mkdir -p .ssh\""
-    action :run
-  end
-
-  execute "add .ssh dir for user #{sftp_site[:upload][:user]}" do
-    command "sudo su - #{sftp_site[:upload][:user]} -c \"mkdir -p .ssh\""
-    action :run
-  end
-
-  execute "chmod .ssh dir for user #{sftp_site[:upload][:user]}" do
-    command "sudo su - #{sftp_site[:upload][:user]} -c \"chmod 700 .ssh\""
-    action :run
-  end
-
-  execute "chmod .ssh dir for user #{sftp_site[:download][:user]}" do
-    command "sudo su - #{sftp_site[:download][:user]} -c \"chmod 700 .ssh\""
-    action :run
-  end
-
-  template "/home/#{sftp_site[:download][:user]}/.ssh/authorized_keys" do
+  template "/home/#{sftp_site[:user]}/.ssh/authorized_keys" do
     backup false
     source 'authorized_keys.erb'
-    owner sftp_site[:download][:user]
-    group sftp_site[:download][:user]
-    variables :public_key => sftp_site[:download][:public_key] || ""
+    owner sftp_site[:user]
+    group sftp_site[:user]
+    variables :public_key => sftp_site[:public_key] || ""
     mode 0600
   end
 
-  template "/home/#{sftp_site[:upload][:user]}/.ssh/authorized_keys" do
-    backup false
-    source 'authorized_keys.erb'
-    owner sftp_site[:upload][:user]
-    group sftp_site[:upload][:user]
-    variables :public_key => sftp_site[:upload][:public_key] || ""
-    mode 0600
-  end
-
-  base_repo = File.join(sftp_base_dir, sftp_site[:home])
+  base_repo = File.join(sftp_base_dir, sftp_site[:user])
   execute "create sftp repo #{base_repo}" do
     command "sudo mkdir -p #{base_repo}"
     action :run
   end
 
 
-  # Download Folder
-  download_dir = File.join(base_repo, sftp_site[:download][:folder])
-  execute "create sftp repo #{download_dir}" do
-    command "sudo mkdir -p #{download_dir}"
+  # SFTP Folder
+  sftp_dir = File.join(base_repo, sftp_site[:folder])
+  execute "create sftp repo #{sftp_dir}" do
+    command "sudo mkdir -p #{sftp_dir}"
     action :run
   end
 
-  execute "chown #{download_dir}" do
-    command "sudo chown #{sftp_site[:download][:user]}:#{sftp_site[:upload][:user]} #{download_dir}"
+  execute "chown #{sftp_dir}" do
+    command "sudo chown #{sftp_site[:user]}:#{sftp_site[:user]} #{sftp_dir}"
     action :run
   end
 
-  execute "chmod #{download_dir}" do
-    command "sudo chmod #{node[:sftp_permission]} -R #{download_dir}"
+  execute "chmod #{sftp_dir}" do
+    command "sudo chmod #{node[:sftp_permission]} -R #{sftp_dir}"
     action :run
   end
-
-
-  # Upload Folder
-  upload_dir = File.join(base_repo, sftp_site[:upload][:folder])
-  execute "create sftp repo #{upload_dir}" do
-    command "sudo mkdir -p #{upload_dir}"
-    action :run
-  end
-
-  execute "chown #{upload_dir}" do
-    command "sudo chown #{sftp_site[:upload][:user]}:#{sftp_site[:download][:user]} #{upload_dir}"
-    action :run
-  end
-
-  execute "chmod #{upload_dir}" do
-    command "sudo chmod #{node[:sftp_permission]} -R #{upload_dir}"
-    action :run
-  end
-
-
-
 end
 
 service "sshd" do
